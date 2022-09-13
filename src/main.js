f6ad7b545b2e0cccccd1d48f32c0ae7c678cee79
qrai <sergeikab77@gmail.com>
2022-09-13 23:20:21 +0400

    Setup Vue 3, basic SCSS

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// Vue
+import { createApp } from 'vue'
+
+// Styles
+import './assets/main.scss'
+import './assets/theme.scss'
+
+// App
+import App from './App.vue'
+
+// Mount!
+createApp(App).mount('#app')
